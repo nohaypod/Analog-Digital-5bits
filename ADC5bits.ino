def8ece7c02b4139099359c9cad0ac3a9b33638e
Aldo Ortega González <aeledeo.9@gmail.com>
2023-11-13 18:14:50 -0600

    Update ADC5bits.ino

--- ADC5bits.ino
+++ ADC5bits.ino
@@ -1,22 +1,26 @@
-int sensorValue = 0;
-int mapeado=0;
+int sensorValue = 0;//variable para leer sensor o potenciometro como divisor de voltaje
+int mapeado=0;//variable para guardar la varible mapeada a la cantidad de bits corrrespondientes
 
 void setup()
 {
-  pinMode(A0, INPUT);
-  Serial.begin(9600);
-}
+  pinMode(A0, INPUT);//se lee el sensor en el analógico A0
+  pinMode(0, OUTPUT);//Input to activate relay 1 bit menos significativo
+  pinMode(1, OUTPUT);//Input to activate relay 2
+  pinMode(2, OUTPUT);//Input to activate relay 3
+  pinMode(3, OUTPUT);//Input to activate relay 4 bit más significativo
+
+ }
 
 void loop()
 {
   // read the input on analog pin 0:
   sensorValue = analogRead(A0);
-  mapeado = map(sensorValue, 0, 1023, 0, 15);
+  mapeado = map(sensorValue, 0, 1023, 0, 15);//se mapea a 15 para 4bits 
 
-  // print out the value you read:
-  Serial.println(mapeado);
-  for (int i = 0; i <= 5; i++) {
-  digitalWrite(i, (mapeado >> (i - 2)) & 1);
-}
-  delay(10); // Delay a little bit to improve simulation performance
+ 
+  for (int i = 0; i <= 4; i++) {
+  digitalWrite(i, (mapeado >> (i )) & 1);
+	}
+  
+  delay(1); // delaytalvez inecesario
 }
